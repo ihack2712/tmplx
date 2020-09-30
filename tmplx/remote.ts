c2897e0188a95581b38f29ea5af5812121c9136e
James Bradlee <james@ihacks.dev>
2020-09-30 12:00:45 +0200

    :rocket: Changed the remote root version.

--- tmplx/remote.ts
+++ tmplx/remote.ts
@@ -1,4 +1,4 @@
 // Exports
-export * from "https://deno.land/x/tmplx@root-v0.1.5/tmplx/TemplateRoot.ts";
-export * from "https://deno.land/x/tmplx@root-v0.1.5/tmplx/Core.ts";
-export * from "https://deno.land/x/tmplx@root-v0.1.5/tmplx/IEngine.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/TemplateRoot.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/Core.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/IEngine.ts";
