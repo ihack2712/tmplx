5c9f6b5189071dba2db5a2d2cec8f839dc4d5f03
James Bradlee <james@ihacks.dev>
2020-10-01 07:51:13 +0200

    :pencil: Updated the remote version.

--- tmplx/remote.ts
+++ tmplx/remote.ts
@@ -1,4 +1,4 @@
 // Exports
-export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/TemplateRoot.ts";
-export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/Core.ts";
-export * from "https://deno.land/x/tmplx@root-v0.1.6/tmplx/IEngine.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.7/tmplx/TemplateRoot.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.7/tmplx/Core.ts";
+export * from "https://deno.land/x/tmplx@root-v0.1.7/tmplx/IEngine.ts";
