58df1826f4bbd4471255a7fe8bd1cbe469f31eb8
James Bradlee <james@ihacks.dev>
2020-10-01 07:50:33 +0200

    :sparkles: Added getEngineVersion() and command() methods to Engine interface.

--- tmplx/IEngine.ts
+++ tmplx/IEngine.ts
@@ -1,9 +1,19 @@
+// Imports
+import type { Command } from "../vendors/cliffy/command.ts";
+
 /**
  * An engine interface.
  */
 export interface IEngine
 {
 	
+	/** Get the engine version. */
+	getEngineVersion (): string;
 	
+	/**
+	 * Inject commands onto the Command-Iine Interface.
+	 * @param commands The commands to inject.
+	 */
+	command (...commands: Command[]): IEngine;
 	
 }
